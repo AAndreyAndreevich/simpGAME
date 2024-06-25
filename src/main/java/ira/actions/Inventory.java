81274120a2fd504fc81b36b5e1e217e0274c2e34
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-06-25 19:49:53 +0300

    0.0

--- src/main/java/ira/actions/Inventory.java
+++ src/main/java/ira/actions/Inventory.java
@@ -13,47 +13,50 @@ public class Inventory {
         Scanner scanner = new Scanner(System.in);
         System.out.println("пишите: ");
         currentCommand = scanner.nextLine();
-        switch (currentCommand) {
-            case "1":
-                int i = 0;
-                for (SomeItem item : player.getInventory()) {
-                    i++;
-                    System.out.println(i + " : " + item.getName());
-                }
-                Inventory.inventory(player);
-            case "2":
-                currentItem = scanner.nextLine();
-                for (SomeItem item : player.getInventory()) {
-                    if (item.getName().equals(currentItem)) {
-                        player.equip(item);
-                        Inventory.inventory(player);
+        while (!currentCommand.equals("6")) {
+            switch (currentCommand) {
+                case "1":
+                    System.out.println(player.getInventory());
+                    break;
+                case "2":
+                    currentItem = scanner.nextLine();
+                    for (SomeItem item : player.getInventory()) {
+                        if (item.getName().equals(currentItem)) {
+                            player.equip(item);
+                            break;
+                        }
                     }
-                }
-                Inventory.inventory(player);
-            case "3":
-                currentItem = scanner.nextLine();
-                for (SomeItem item : player.getInventory()) {
-                    if (item.getName().equals(currentItem)) {
-                        player.getInventory().remove(item);
-                        Inventory.inventory(player);
+                    break;
+                case "3":
+                    currentItem = scanner.nextLine();
+                    for (SomeItem item : player.getInventory()) {
+                        if (item.getName().equals(currentItem)) {
+                            player.getInventory().remove(item);
+                            break;
+                        }
                     }
-                }
-                Inventory.inventory(player);
-            case "4":
-                currentItem = scanner.nextLine();
-                //снятие
-            case "5":
-                System.out.println("Броня : " + player.getArmor().getName());
-                System.out.println("Оружие : " + player.getWeapon().getName());
-                System.out.println("Баланс : " + player.getMoney());
-                break;
-            case "6":
-                System.out.println("Выход есть");
-                break;
-            default:
-                System.out.println("Вы выбрали не то");
-                Inventory.inventory(player);
+                    break;
+                case "4"://пока выдаются ошибки на снятие
+                    currentItem = scanner.nextLine();
+                    if (player.getArmor().getName().equals(currentItem)){
+                        player.unEquip(player.getArmor());
+                    } else if (player.getWeapon().getName().equals(currentItem)) {
+                        player.unEquip(player.getWeapon());
+                    }
+                    break;
+                case "5":
+                    if (player.getArmor() != null) System.out.println("Броня : " + player.getArmor().getName());
+                    else System.out.println("Нет брони");
+                    if (player.getWeapon() != null) System.out.println("Оружие : " + player.getWeapon().getName());
+                    else System.out.println("Нет оружия");
+                    System.out.println("Баланс : " + player.getMoney());
+                    break;
+                default:
+                    System.out.println("Вы выбрали не то");
+                    continue;
+            }
+            System.out.println("пишите:");
+            currentCommand = scanner.nextLine();
         }
-
     }
 }
