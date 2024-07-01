396cf9827519cdba6cb384b1eef5dd56000b6c7a
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-01 13:28:17 +0300

    0.0

--- src/main/java/ira/models/Monster.java
+++ src/main/java/ira/models/Monster.java
@@ -2,13 +2,11 @@ package ira.models;
 
 import ira.enums.EnumSomeStatus;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Monster extends SomePerson {
 
